7e3238fc2ff384961535dbeb788b753a72366d63
clintjeff2 <clintjeff2@gmail.com>
2026-03-26 07:07:31 +0100

    feat: export Spinner and LoadingState from ui barrel

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,4 +1,7 @@
-
 export { Icon } from "./Icon";
 export type { IconProps } from "./Icon";
 export { BentoCard } from "./BentoCard";
+export { Spinner } from "./Spinner";
+export type { SpinnerProps } from "./Spinner";
+export { LoadingState } from "./LoadingState";
+export type { LoadingStateProps } from "./LoadingState";
